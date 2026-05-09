a4fe33b59bf1c17e0aa224f6ff96ba4ae8026a49
EasyIndividuality <michaeldestinyfk@gmail.com>
2026-05-09 19:21:46 +0000

    implemented the playRound function

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 function getComputerChoice() {
-  const randomNumber = Math.random();
-  if (randomNumber <= 1 / 3) {
-    return "rock";
-  } else if (randomNumber <= 2 / 3) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
+	const randomNumber = Math.random();
+	if (randomNumber <= 1 / 3) {
+		return "rock";
+	} else if (randomNumber <= 2 / 3) {
+		return "paper";
+	} else {
+		return "scissors";
+	}
 }
 
 function getHumanChoice() {
-  const userChoice = prompt("What's Your Choice (rock, paper, scissors) ?");
-  return userChoice.toLocaleLowerCase();
+	const userChoice = prompt("What's Your Choice (rock, paper, scissors) ?");
+	return userChoice.trim().toLocaleLowerCase();
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = getHumanChoice();
-  computerChoice = getComputerChoice();
+function playRound() {
+	let humanChoice = getHumanChoice();
+	let computerChoice = getComputerChoice();
 
-  if (computerChoice === "rock" && humanChoice === "rock") {
-    console.log("It's a TIE : Try again");
-  } else if (computerChoice === "rock" && humanChoice === "paper") {
-    console.log("You Win! : Paper beats Rock");
-    humanChoice++;
-  } else if (computerChoice === "rock" && humanChoice === "scissors") {
-    console.log("You LOSE! : Rock beats Scissors");
-    computerScore++;
-  }
+	if (computerChoice === "rock" && humanChoice === "rock") {
+		isATie();
+	} else if (computerChoice === "rock" && humanChoice === "paper") {
+		youWin(computerChoice, humanChoice);
+	} else if (computerChoice === "rock" && humanChoice === "scissors") {
+		youLose(computerChoice, humanChoice);
+	} else if (computerChoice === "paper" && humanChoice === "rock") {
+		youLose(computerChoice, humanChoice);
+	} else if (computerChoice === "paper" && humanChoice === "paper") {
+		isATie();
+	} else if (computerChoice === "paper" && humanChoice === "scissors") {
+		youWin(computerChoice, humanChoice);
+	} else if (computerChoice === "scissors" && humanChoice === "rock") {
+		youWin(computerChoice, humanChoice);
+	} else if (computerChoice === "scissors" && humanChoice === "paper") {
+		youLose(computerChoice, humanChoice);
+	} else if (computerChoice === "scissors" && humanChoice === "scissors") {
+		isATie();
+	}
 }
 
 playRound();
+
+function youWin(cpu, p1) {
+	console.log(`You Win! : ${p1} beats ${cpu}`);
+	humanScore++;
+}
+function youLose(cpu, p1) {
+	console.log(`You LOSE! : ${cpu} beats ${p1}`);
+	computerScore++;
+}
+function isATie() {
+	console.log("It's a TIE : Try again");
+}
