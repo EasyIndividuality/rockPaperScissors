a867902ca3a37750d506fe945e367d5d1524d484
EasyIndividuality <michaeldestinyfk@gmail.com>
2026-05-14 13:51:41 +0000

    completed the game logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", playGame);
 function getComputerChoice() {
 	const randomNumber = Math.random();
 	if (randomNumber <= 1 / 3) {
@@ -14,6 +15,7 @@ function getHumanChoice() {
 	return userChoice.trim().toLocaleLowerCase();
 }
 
+let roundCount = 0;
 let humanScore = 0;
 let computerScore = 0;
 
@@ -23,35 +25,58 @@ function playRound() {
 
 	if (computerChoice === "rock" && humanChoice === "rock") {
 		isATie();
+		playGame();
 	} else if (computerChoice === "rock" && humanChoice === "paper") {
 		youWin(computerChoice, humanChoice);
+		playGame();
 	} else if (computerChoice === "rock" && humanChoice === "scissors") {
 		youLose(computerChoice, humanChoice);
+		playGame();
 	} else if (computerChoice === "paper" && humanChoice === "rock") {
 		youLose(computerChoice, humanChoice);
+		playGame();
 	} else if (computerChoice === "paper" && humanChoice === "paper") {
 		isATie();
+		playGame();
 	} else if (computerChoice === "paper" && humanChoice === "scissors") {
 		youWin(computerChoice, humanChoice);
+		playGame();
 	} else if (computerChoice === "scissors" && humanChoice === "rock") {
 		youWin(computerChoice, humanChoice);
+		playGame();
 	} else if (computerChoice === "scissors" && humanChoice === "paper") {
 		youLose(computerChoice, humanChoice);
+		playGame();
 	} else if (computerChoice === "scissors" && humanChoice === "scissors") {
 		isATie();
+		playGame();
 	}
 }
 
-playRound();
+function playGame() {
+	if (roundCount < 5) {
+		playRound();
+	} else {
+		if (humanScore > computerScore) {
+			alert("YOU WIN!")
+		}else if(computerScore > humanScore){
+			alert("YOU LOSE!")
+		}
+		alert("DRAW!")
+	}
+}
 
 function youWin(cpu, p1) {
 	console.log(`You Win! : ${p1} beats ${cpu}`);
 	humanScore++;
+	roundCount++;
 }
 function youLose(cpu, p1) {
 	console.log(`You LOSE! : ${cpu} beats ${p1}`);
 	computerScore++;
+	roundCount++;
 }
 function isATie() {
 	console.log("It's a TIE : Try again");
+	roundCount++;
 }
